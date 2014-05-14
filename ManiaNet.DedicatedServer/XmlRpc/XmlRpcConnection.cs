fac03fe019183a9f4ad2cfe80991c5e732fe3418
Arne Kiesewetter <banane1996@gmx.net>
2014-05-14 18:34:31 +1200

    For some reason not working test.

---
+++ ManiaNet.DedicatedServer/XmlRpc/XmlRpcConnection.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ManiaNet.DedicatedServer.XmlRpc
+{
+    public class XmlRpcConnection
+    {
+        private Protocol protocol;
+        private StreamReader reader;
+        private uint requestHandle = 0;
+        private Stream stream;
+        private StreamWriter writer;
+
+        public XmlRpcConnectionConfig Config { get; private set; }
+
+        public XmlRpcConnection(XmlRpcConnectionConfig config)
+        {
+            Config = config;
+        }
+
+        public void Connect()
+        {
+            stream = new TcpClient(Config.Address, Config.Port).GetStream();
+            reader = new StreamReader(stream, Encoding.UTF8);
+            writer = new StreamWriter(stream, Encoding.UTF8);
+
+            byte[] protocolNameLengthBytes = new byte[4];
+            stream.Read(protocolNameLengthBytes, 0, 4);
+            uint protocolNameLength = BitConverter.ToUInt32(protocolNameLengthBytes, 0);
+
+            if (protocolNameLength > 64)
+                throw new Exception("Wrong Low-Level Protocol Header");
+
+            char[] protocolName = new char[protocolNameLength];
+            reader.ReadBlock(protocolName, 0, (int)protocolNameLength);
+            switch (new string(protocolName))
+            {
+                case "GBXRemote 1":
+                    protocol = Protocol.GbxRemoteOne;
+                    break;
+
+                case "GBXRemote 2":
+                    protocol = Protocol.GbxRemoteTwo;
+                    break;
+
+                default:
+                    throw new Exception("Wrong Low-Level Protocol Version");
+            }
+        }
+
+        public void Receive()
+        {
+            char[] chars = new char[1];
+            while (true)
+            {
+                reader.ReadBlock(chars, 0, 1);
+                Console.WriteLine(chars[0]);
+            }
+        }
+
+        public void Send(string request)
+        {
+            requestHandle++;
+            List<byte> bytes = new List<byte>(BitConverter.GetBytes((uint)request.Length));
+
+            //GBXRemote 1 only takes the length
+            if (protocol == Protocol.GbxRemoteTwo)
+            {
+                bytes.AddRange(BitConverter.GetBytes((uint)1));
+            }
+
+            //stream.Write(bytes.ToArray(), 0, bytes.Count);
+            //writer.Write(request);
+
+            byte[] requestBytes = new byte[request.Length];
+            Encoding.ASCII.GetEncoder().GetBytes(request.ToCharArray(), 0, request.Length, requestBytes, 0, true);
+            bytes.AddRange(requestBytes);
+            foreach (byte b in bytes)
+            {
+                Console.Write(b + "-");
+            }
+            Console.WriteLine();
+            stream.Write(bytes.ToArray(), 0, bytes.Count);
+            stream.Flush();
+        }
+
+        private enum Protocol
+        {
+            GbxRemoteOne = 1,
+            GbxRemoteTwo = 2
+        }
+    }
+}
---
+++ ManiaNet.DedicatedServer/XmlRpc/XmlRpcConnectionConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ManiaNet.DedicatedServer.XmlRpc
+{
+    public class XmlRpcConnectionConfig
+    {
+        public string Address { get; private set; }
+
+        public string Password { get; private set; }
+
+        public int Port { get; private set; }
+
+        public string Username { get; private set; }
+
+        public XmlRpcConnectionConfig(string address = "localhost", int port = 5000, string username = "SuperAdmin", string password = "SuperAdmin")
+        {
+            Address = address;
+            Port = port;
+            Username = username;
+            Password = password;
+        }
+    }
+}
