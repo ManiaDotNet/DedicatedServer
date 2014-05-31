3a1f8d1a494099cbd1d0947ef7ce68e2fd59da00
Arne Kiesewetter <banane1996@gmx.net>
2014-05-31 18:55:34 +1200

    Remove XmlRpc prefix from Structs; Add some; Add snippet for Struct ParseXml method

--- ManiaNet.Tests/DedicatedServer/XmlRpc/Types/StructTests.cs
+++ ManiaNet.Tests/DedicatedServer/XmlRpc/Types/StructTests.cs
@@ -30,14 +30,14 @@ namespace ManiaNet.Tests.DedicatedServer.XmlRpc.Types
 
             #endregion XML
 
-            var applicationVersionStruct = new XmlRpcStruct<ManiaNet.DedicatedServer.XmlRpc.Types.Structs.XmlRpcApplicationVersionStruct>();
+            var applicationVersionStruct = new XmlRpcStruct<ManiaNet.DedicatedServer.XmlRpc.Types.Structs.ApplicationVersionStruct>();
             applicationVersionStruct.ParseXml(XDocument.Parse(xml).Root);
 
-            Assert.AreEqual<string>("ManiaPlanet", applicationVersionStruct.Value.Name.Value);
-            Assert.AreEqual<string>("TMCanyon", applicationVersionStruct.Value.TitleId.Value);
-            Assert.AreEqual<string>("3.3.0", applicationVersionStruct.Value.Version.Value);
-            Assert.AreEqual<string>("2014-04-30_00_04", applicationVersionStruct.Value.Build.Value);
-            Assert.AreEqual<string>("2011-08-01", applicationVersionStruct.Value.ApiVersion.Value);
+            Assert.AreEqual<string>("ManiaPlanet", applicationVersionStruct.Value.name.Value);
+            Assert.AreEqual<string>("TMCanyon", applicationVersionStruct.Value.titleId.Value);
+            Assert.AreEqual<string>("3.3.0", applicationVersionStruct.Value.version.Value);
+            Assert.AreEqual<string>("2014-04-30_00_04", applicationVersionStruct.Value.build.Value);
+            Assert.AreEqual<string>("2011-08-01", applicationVersionStruct.Value.apiVersion.Value);
         }
     }
 }
