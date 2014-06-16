0dc25a1575d2a320c47cdf705153b1af05f75dd1
Arne Kiesewetter <banane1996@gmx.net>
2014-06-16 23:54:43 +1200

    Now targetting .NET 4.5

--- ManiaNet.DedicatedServer/XmlRpc/MethodCalls/GetCurrentRankingForLogin.cs
+++ ManiaNet.DedicatedServer/XmlRpc/MethodCalls/GetCurrentRankingForLogin.cs
@@ -12,11 +12,28 @@ namespace ManiaNet.DedicatedServer.XmlRpc.MethodCalls
     public sealed class GetCurrentRankingForLogin : MethodCall<XmlRpcString, string, XmlRpcStruct<RankingStruct>, RankingStruct>
     {
         /// <summary>
+        /// Gets or sets the login of the player to get the ranking for.
+        /// </summary>
+        public string Login
+        {
+            get { return param1.Value; }
+            set { param1.Value = value; }
+        }
+
+        /// <summary>
         /// Gets the name of the method this call is for.
         /// </summary>
         public override string MethodName
         {
             get { return "GetCurrentRankingForLogin"; }
         }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="ManiaNet.DedicatedServer.XmlRpc.MethodCalls.GetCurrentRankingForLogin"/> class for the given login.
+        /// </summary>
+        /// <param name="login">The login of the player to get the ranking for.</param>
+        public GetCurrentRankingForLogin(string login)
+            : base(login)
+        { }
     }
 }
